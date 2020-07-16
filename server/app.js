e2b728fe1333d9caeefe2ad9c5ddd490ff0ccd15
unknown <naviworld98@gmail.com>
2020-07-16 20:49:12 +0530

    server side

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const {MONGOURL} = require('../config/key')
+const PORT =  5000
+var cors = require("cors");
+
+mongoose.connect(MONGOURL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+})
+mongoose.connection.on('connected',()=>{
+   console.log("connected to mongodb")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("error in connection",err)
+ })
+
+require('./model/user')
+
+app.use(express.json())
+app.use(cors());
+app.use(require('./routes/user'));
+
+app.listen(PORT,()=>{
+    console.log("server on PORT",PORT)
+})
+
